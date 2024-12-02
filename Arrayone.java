e551e29ee798ad31ead30b3eebb479cd7827a76b
Harshit Jain <144523189+harshit0729@users.noreply.github.com>
2024-12-02 10:42:23 +0530

    Add files via upload

---
+++ Arrayone.java
@@ -0,0 +1,20 @@
+package hj;
+
+public class Arrayone {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,2,3,4,5};
+		//for each
+		for (int i: arr) {
+			System.out.println(i);
+		}
+		System.out.println("\n");
+		// for loop
+	
+for(int i=0;i<=4;i++) {
+	System.out.println(arr[i]);
+}
+	}
+
+}
